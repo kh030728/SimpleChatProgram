b2eb3854aa3e500038988532f37a76c39bfd5a30
kh030728 <32125724+kh030728@users.noreply.github.com>
2019-08-02 22:19:28 +0900

    Update LogOnWindow.xaml
    정리되지 않은 UI를 정리

--- Client_cs/SimpleChatClient/SimpleChatClient/LogOnWindow.xaml.cs
+++ Client_cs/SimpleChatClient/SimpleChatClient/LogOnWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace SimpleChatClient
                 {
                     LOW_tB_statusMsg.Text = "IP주소를 입력해주세요.";
                     LOW_tB_statusMsg.Background = new SolidColorBrush(Colors.Red);
+                    LOW_tB_statusMsg.Visibility = Visibility.Visible;
                     buttonExecuteFlag = false;
                     return;
                 }
@@ -32,11 +33,11 @@ namespace SimpleChatClient
                 {
                     LOW_tB_statusMsg.Text = "닉네임을 입력해주세요.";
                     LOW_tB_statusMsg.Background = new SolidColorBrush(Colors.Red);
+                    LOW_tB_statusMsg.Visibility = Visibility.Visible;
                     buttonExecuteFlag = false;
                     return;
                 }
-                LOW_tB_statusMsg.Text = "연결 시도중";
-                LOW_tB_statusMsg.Background = new SolidColorBrush(Colors.RoyalBlue);
+
                 tcpCli = new TCPCli();
                 string IPAddr = tb_IPAddr.Text;
                 string NickName = tb_NickName.Text;
@@ -46,12 +47,14 @@ namespace SimpleChatClient
                     tcpCli.requestRoomInfo();
                     LOW_tB_statusMsg.Text = "연결 완료";
                     LOW_tB_statusMsg.Background = new SolidColorBrush(Colors.RoyalBlue);
+                    LOW_tB_statusMsg.Visibility = Visibility.Visible;
                 }
                 else
                 {
 
                     LOW_tB_statusMsg.Text = "연결 실패";
                     LOW_tB_statusMsg.Background = new SolidColorBrush(Colors.Red);
+                    LOW_tB_statusMsg.Visibility = Visibility.Visible;
                 }
                 buttonExecuteFlag = false;
             }
@@ -59,7 +62,7 @@ namespace SimpleChatClient
         private void Tb_IPAddr_GotFocus(object sender, RoutedEventArgs e)
         {
             LOW_tB_statusMsg.Text = "";
-            LOW_tB_statusMsg.Background = new SolidColorBrush(Colors.RoyalBlue);
+            LOW_tB_statusMsg.Visibility = Visibility.Hidden;
         }
     }
 }
