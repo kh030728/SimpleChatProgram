9b189db228fe09640866687b6174957ee0acaaeb
kh030728 <32125724+kh030728@users.noreply.github.com>
2019-08-14 21:06:25 +0900

    19.08.14일 까지의 결과물

--- Client_cs/SimpleChatClient/SimpleChatClient/ChatWindow.xaml.cs
+++ Client_cs/SimpleChatClient/SimpleChatClient/ChatWindow.xaml.cs
@@ -19,9 +19,54 @@ namespace SimpleChatClient
     /// </summary>
     public partial class ChatWindow : Window
     {
+        NetworkSystem ns;
         public ChatWindow()
         {
+            ns = NetworkSystem.Instance;
             InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            sendButton.IsEnabled = false;
+            string tmp = tbx_chatInput.Text;
+            tbx_chatInput.Text = "";
+            AddLog(tmp);
+            sendButton.IsEnabled = true;
+        }
+        private void RecvThread()
+        {
+            while(true)
+            {
+                string Msg = ns.RecvMsg();
+                if(Msg.Contains("") == true)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+        
+        private void AddLog(string Msg)
+        {
+            chatitem _item = new chatitem();
+            _item.nameTextBlock.Text = "김광현";
+            _item.messageTextBlock.Text = Msg;
+            chatStackPanel.Children.Add(_item);
+            sv_Chatlog.ScrollToBottom();
+        }
+
+        private void Tbx_chatInput_EnterKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Return)
+            {
+                string tmp = tbx_chatInput.Text;
+                tbx_chatInput.Text = "";
+                AddLog(tmp);
+            }
         }
     }
 }
