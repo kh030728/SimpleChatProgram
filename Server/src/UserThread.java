9ef03b167c4c5ba25f6cbd6d6c100cb2364382a8
wo-hu <sha9092@naver.com>
2019-08-23 23:35:15 +0900

    Update Server
     - 닉네임 받을 필요 없게 1차 수정
     - 오타 수정

--- Server/src/UserThread.java
+++ Server/src/UserThread.java
@@ -30,10 +30,6 @@ public class UserThread extends Thread {
 	public void run() {
 		UsersInfo userInstance = UsersInfo.getInstance();
 		RoomsInfo roomInstance = RoomsInfo.getInstance();
-		/*
-		 * Room r1 = new Room("1번방인데수", "ㄱㄱㅎ"); roomInstance.addRoom(r1); Room r2 = new
-		 * Room("2번방이지요", "kkh"); roomInstance.addRoom(r2);
-		 */
 
 		try {
 			BufferedReader Breader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -92,7 +88,7 @@ public class UserThread extends Thread {
 				}
 
 				// 방 생성
-				else if (str.contains("REQUEST_CREATE_ROOM%$%")) {
+				else if (str.contains("REQUEST_CREATE_ROOM%$%")) { // (유저 닉네임 받을 필요 없음(수정 필요))
 					System.out.println("방생성 요청 메세지 수신 완료 / 수신 메세지 : " + str);
 					String[] createRoomStr = str.split("\\%\\$\\%"); // [0] : 요청 메세지, [1] : 방 제목, [2] : 요청 유저
 					System.out.println("메세지 분리 확인용 - 0 : " + createRoomStr[0] + " 1 : " + createRoomStr[1] + " 2 : " + createRoomStr[2]);
@@ -115,7 +111,7 @@ public class UserThread extends Thread {
 					str = Breader.readLine();
 					System.out.println("추가 메세지 확인 : " + str);
 					if(str.equals("READY_FOR_JOIN")) {
-						Pwriter.println("USERS%$%" + createRoomStr[2]);
+						Pwriter.println("USERS%$%" + userInfo.nickName);
 						Pwriter.flush();
 					}
 					for (int i = 0; i < userInstance.getSizeInfo(); i++) { // 방 생성 후 갱신된 방 정보 재 전송
@@ -131,24 +127,26 @@ public class UserThread extends Thread {
 				}
 
 				// 방 참여
-				else if (str.contains("REQUEST_JOIN_ROOM%$%")) {
+				else if (str.contains("REQUEST_JOIN_ROOM%$%")) { // (참여할 유저 필요 없음(수정 필요))
 					System.out.println("방 참여 메세지 수신 완료 / 수신 메세지 : " + str);
 					String[] joinRoomStr = str.split("\\%\\$\\%"); // [0] : 요청 메세지, [1] : 참여할 유저, [2] : 참여할 방 번호
 					System.out.println("메세지 분리 확인용 - 0 : " + joinRoomStr[0] + " 1 : " + joinRoomStr[1] + " 2 : " + joinRoomStr[2]);
-					roomInstance.getRoomInfo(Integer.parseInt(joinRoomStr[2])).AddEntry(userInfo); // 방 정보에 참여 유저 넣기
-					userInfo.roomNu = Integer.parseInt(joinRoomStr[2]);
+					int roomNu = Integer.parseInt(joinRoomStr[2]);  // 방 번호
+					roomInstance.getRoomInfo(roomNu).AddEntry(userInfo); // 방 정보에 참여 유저 넣기
+					userInfo.roomNu = roomNu; // 유저 정보의 현재 방 번호 변경
 					Pwriter.println("FINISH_JOIN");
 					Pwriter.flush();
+					
 					str = null;
 					str = Breader.readLine();
 					System.out.println("참여 확인용" + str);
 					if(str.equals("READY_FOR_JOIN")) { // 후 처리 전 메세지 받기
 						// 참여자 목록 보내기
 						String users = "USERS";
-						for (int i = 0; i < roomInstance.getRoomInfo(Integer.parseInt(joinRoomStr[2])).entry.size(); i++) {
+						for (int i = 0; i < roomInstance.getRoomInfo(roomNu).entry.size(); i++) {
 							System.out.println("참여자 목록 가져오는 중 / 현재 진행도 : " + users);
 							users += "%$%";
-							users += roomInstance.getRoomInfo(Integer.parseInt(joinRoomStr[2])).entry.get(i).nickName;
+							users += roomInstance.getRoomInfo(roomNu).entry.get(i).nickName;
 							System.out.println("참여자 목록 가져오는 중 / 현재 완료된 진행 : " + users);
 						}
 						users += "\r\n";
@@ -156,21 +154,22 @@ public class UserThread extends Thread {
 						Pwriter.flush();
 						
 						// 참여한 방에 참여자 정보 전달
-						ArrayList<User> joinUsers = roomInstance.getRoomInfo(Integer.parseInt(joinRoomStr[2])).entry; // 참여한 방의 유저 리스트
+						ArrayList<User> joinUsers = roomInstance.getRoomInfo(roomNu).entry; // 참여한 방의 유저 리스트
 						System.out.println("참여한 방의 갱신된 유저 목록 : " + joinUsers);
 						for (int i = 0; i < joinUsers.size(); i++) { // 채팅 전송
 							System.out.println("참여한 방의 유저들에게 참여자 정보 전송중 / 현재 " + i + "번 유저에게 전송 대기 중");
 							PrintWriter sendChat = new PrintWriter(joinUsers.get(i).socket.getOutputStream());
-							sendChat.println("JOIN_USER%$%" + joinRoomStr[1]);
+							sendChat.println("JOIN_USER%$%" + userInfo.nickName);
 							sendChat.flush();
 							System.out.println("참여한 방의 유저들에게 참여자 정보 전송중 / 현재 " + i + "번 유저까지 전송 완료 상태");
 						}
 						System.out.println("참여한 방의 유저에게 새로운 참여자 전송 완료");
+						
 						System.out.println("전체 유저에게 인원수가 변경된 방 알림");
 						for (int i = 0; i < userInstance.getSizeInfo(); i++) {
 							System.out.println(i + "번 유저에게 전송 준비 중");
 							PrintWriter sendChangePeople = new PrintWriter(userInstance.getUserInfo(i).socket.getOutputStream());
-							sendChangePeople.println("NOTIFY_CHAGE_ROOM%$%" + joinRoomStr[2] + "%$%" + roomInstance.getRoomInfo(Integer.parseInt(joinRoomStr[2])).entry.size());
+							sendChangePeople.println("NOTIFY_CHAGE_ROOM%$%" + roomNu + "%$%" + roomInstance.getRoomInfo(roomNu).entry.size());
 							sendChangePeople.flush();
 							System.out.println(i + "번 유저까지 전송 완료");
 						}
@@ -181,12 +180,12 @@ public class UserThread extends Thread {
 				}
 
 				// 방 나가기
-				else if (str.contains("REQUEST_OUT_ROOM%$%")) {
+				else if (str.contains("REQUEST_OUT_ROOM%$%")) { // (나간 유저 필요없음 (수정 필요))
 					System.out.println("방 나가기 메세지 요청 수신 완료 / 수신된 메세지 : " + str);
 					String[] outRoomStr = str.split("\\%\\$\\%"); // [0] : 요청 메세지, [1] : 나간 유저
-					System.out.println("메세지 분리 확인용 - 0 : " + outRoomStr[0] + " 1 : " + outRoomStr[1] + " 2 : " + outRoomStr[2]);
-					roomInstance.getRoomInfo(Integer.parseInt(outRoomStr[2])).RemoveEntry(userInfo); // 해당 방의 유저 목록에서 나간 유저 삭제
-					Pwriter.println("OUR_ROOM_OK");
+					System.out.println("메세지 분리 확인용 - 0 : " + outRoomStr[0] + " 1 : " + userInfo.nickName);
+					roomInstance.getRoomInfo(userInfo.roomNu).RemoveEntry(userInfo); // 해당 방의 유저 목록에서 나간 유저 삭제
+					Pwriter.println("OUT_ROOM_OK");
 					Pwriter.flush();
 					// 나간 유저 정보 전달
 					ArrayList<User> joinUsers = roomInstance.getRoomInfo(userInfo.roomNu).entry; // 참여한 방의 유저 리스트
@@ -195,7 +194,7 @@ public class UserThread extends Thread {
 						for (int i = 0; i < joinUsers.size(); i++) { // 채팅 전송
 							System.out.println(i + "번 유저에게 전송 준비중");
 							PrintWriter sendChat = new PrintWriter(joinUsers.get(i).socket.getOutputStream());
-							sendChat.println("OUT_USER%$%" + outRoomStr[1]);
+							sendChat.println("OUT_USER%$%" + userInfo.nickName);
 							sendChat.flush();
 							System.out.println(i + "번 유저까지 전송 완료");
 						}
